d68e2d44323e7f8c6f76549390d4182a73cdf601
ITTIPON PUNGNAM <ittipon.pung@bulive.net>
2016-02-10 12:40:58 +0700

    gamejam1

---
+++ gamejam/Assets/jump.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetKey (KeyCode.G)) {
+			rb.AddForce(Vector3.up * speed);
+		}
+	}
+}
+
+	
