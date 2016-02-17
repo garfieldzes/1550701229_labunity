0ef5976112edac3dc6873cc9a572303ccbf5fdc5
ITTIPON PUNGNAM <ittipon.pung@bulive.net>
2016-02-17 11:33:01 +0700

    AUDIO

--- Spartanspace/Assets/scrip/PlayerController.cs
+++ Spartanspace/Assets/scrip/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			Instantiate (shot, shotSpawn.transform.position, shotSpawn.transform.rotation);
+			GetComponent<AudioSource>().Play();
 		}
 	}
 	void FixedUpdate ()
